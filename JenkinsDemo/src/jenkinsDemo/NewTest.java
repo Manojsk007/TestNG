65584139d3841957a73d872044831d7ea71f7bec
Manojsk007 <manojsk007@gmail.com>
2016-03-28 00:39:08 +1000

    Adding a TestNG Project

---
+++ JenkinsDemo/src/jenkinsDemo/NewTest.java
@@ -0,0 +1,22 @@
+package jenkinsDemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Hello there");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
